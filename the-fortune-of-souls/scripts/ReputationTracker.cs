839eaf4e4c887071036bc45f1c2f5b99c169a790
Tetratoric <cody.summerson@gmail.com>
2026-02-22 21:50:37 -0600

    added reputation signals

--- the-fortune-of-souls/scripts/ReputationTracker.cs
+++ the-fortune-of-souls/scripts/ReputationTracker.cs
@@ -5,6 +5,9 @@ public partial class ReputationTracker : Node
 {
     public static ReputationTracker Instance;
 
+    [Signal]
+    public delegate void ReputationUpdateEventHandler(int amount);
+
     public int reputation { get; private set; }
 
     public override void _Ready()
@@ -25,6 +28,7 @@ public partial class ReputationTracker : Node
     public void UpdateReputation(int amount)
     {
         reputation += amount;
+        EmitSignal(SignalName.ReputationUpdate, amount);
     }
 
 }
