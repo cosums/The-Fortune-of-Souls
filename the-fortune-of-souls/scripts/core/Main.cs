73995ca47f33b3dbd9e9fa9f2bde273497b6a788
cosums <cody.summerson@gmail.com>
2026-03-06 20:42:10 -0600

    Merge branch 'main' into dialogues

---
+++ the-fortune-of-souls/scripts/core/Main.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+    public override void _EnterTree()
+    {
+        GD.Print("Launching game...");
+    }
+
+    
+    public override void _Ready()
+    {
+        // setup
+    }
+}
